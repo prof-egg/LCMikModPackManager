11cd523296d76171b451f82ad9a7c0e10df79b6f
Cryptic42069 <69015627+Cryptic42069@users.noreply.github.com>
2024-05-20 20:22:58 -0400

    debug can send colored log messages now

--- src/Util/Debug.cs
+++ src/Util/Debug.cs
@@ -4,12 +4,16 @@ namespace MikManager.Util
     {
         public static void LogError(string message, string logger)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"[{GetTimestamp()}] [{logger}/ERROR]: {message}");
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
 
         public static void LogWarning(string message, string logger)
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine($"[{GetTimestamp()}] [{logger}/WARNING]: {message}");
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
 
         public static void LogInfo(string message, string logger)
