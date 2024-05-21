27b9594adfed9e92e475f77bf4075c4c6fbf89f3
Cryptic42069 <69015627+Cryptic42069@users.noreply.github.com>
2024-05-20 20:23:34 -0400

    changes reflect repo handler changes

--- src/MenuStuff/ModPackListMenu.cs
+++ src/MenuStuff/ModPackListMenu.cs
@@ -11,19 +11,15 @@ namespace MikManager.MenuStuff
         {
             Console.WriteLine("MODPACK CONFIG MENU:");
 
-            JArray? configList = RepoHandler.GetModConfigList();
+            string[]? configList = RepoHandler.GetModConfigList();
 
             if (configList == null) 
                 Console.WriteLine("Error: modpack config list was null");
             else 
             {
-                JToken[] configArray = configList.ToArray();
-                for (int i = 0; i < configList.Count; i++) 
-                {
-                    JToken jsonObject = configArray[i];
-                    string? name = jsonObject.Value<string>("name");
-                    Console.WriteLine($"({i + 1}) {name}");
-                }
+                for (int i = 0; i < configList.Length; i++) 
+                    Console.WriteLine($"({i + 1}) {configList[i]}");
+            
                 Console.WriteLine(); // For spacing between options
                 Console.WriteLine($"({this.GetUpperChoiceBound() - 1}) Delete installed mods");
                 Console.WriteLine($"({this.GetUpperChoiceBound()}) Go back");
@@ -50,14 +46,13 @@ namespace MikManager.MenuStuff
 
             // INSTALL MOD PACK SPECIFIED FROM YAML
             // Get modpack file name
-            JArray? configList = RepoHandler.GetModConfigList();
+            string[]? configList = RepoHandler.GetModConfigList();
             if (configList == null) 
             {
                 Console.WriteLine("Error: modpack config list was null");
                 return this;
             }
-            JToken[] configArray = configList.ToArray();
-            string? configFileName = configArray[selection - 1].Value<string>("name");
+            string configFileName = configList[selection - 1];
             if (configFileName == null)
             {
                 Console.WriteLine("Error: couldn't get config file name from user input");
@@ -65,7 +60,8 @@ namespace MikManager.MenuStuff
             }
 
             // Download config
-            RepoHandler.DownloadFileFromRepo($"{RepoHandler.ModConfigPath}/{configFileName}");
+            string relDownloadString = $"{RepoHandler.ModDataDirName}/{RepoHandler.ModPacksDirName}/{RepoHandler.GetGameVersion()}/{configFileName}";
+            RepoHandler.DownloadFileFromRepo(relDownloadString);
 
             // Parse config
             string configPath = RepoHandler.GetDownloadPath(configFileName);
@@ -83,14 +79,14 @@ namespace MikManager.MenuStuff
 
         protected override int GetUpperChoiceBound()
         {
-            JArray? configList = RepoHandler.GetModConfigList();
+            string[]? configList = RepoHandler.GetModConfigList();
             if (configList == null)
                 // This will give the user one option, which is to back out
                 return 1;
             // This will give the user one option per modpack, 
             // one extra option to back out, and another 
             // extra to delete their installed mods
-            return configList.Count + 2;
+            return configList.Length + 2;
         }
 
         protected override int GetLowerChoiceBound()
